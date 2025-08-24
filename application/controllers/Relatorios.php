331d1296ea6d08811b657b55804173947fb92382
felisane <42624636+felisane@users.noreply.github.com>
2025-08-24 12:13:11 +0100

    Bloquear acesso para usuários sem sessão em níveis válidos

--- application/controllers/Relatorios.php
+++ application/controllers/Relatorios.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Relatorios extends CI_Controller {
+class Relatorios extends MY_Controller {
     public function vendas()
     {
         $this->load->model('Venda_model');
